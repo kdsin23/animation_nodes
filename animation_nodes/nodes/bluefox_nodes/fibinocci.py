9cc6d6baf606c61df340aa93cffdea3f990a8400
harisreedhar <harisreedhar@users.noreply.github.com>
2020-03-10 23:35:39 +0530

    added fibinocci node

---
+++ animation_nodes/nodes/bluefox_nodes/fibinocci.py
@@ -0,0 +1,46 @@
+import bpy
+from math import *
+from bpy.props import *
+from ... data_structures import PolySpline, BezierSpline, FloatList, DoubleList
+from ... base_types import AnimationNode
+from ... data_structures import DoubleList
+from ... data_structures import Vector3DList
+
+
+
+class fibinocci(bpy.types.Node, AnimationNode):
+    bl_idname = "an_fibinocci"
+    bl_label = "fibinocci"
+    
+    
+    def create(self):
+        self.newInput("Float", "First", "x1")
+        self.newInput("Float", "Second", "x2")
+        self.newInput("Integer", "count", "count")
+        self.newInput("Float", "MaxValue", "maxValue")
+        
+        self.newOutput("Float List", "result", "res")
+        
+    
+    def execute(self, x1, x2, count, maxValue):
+        if x1 is None or x2 is None:
+            return
+        return self.fibonacci(x1, x2, count, maxValue)
+
+
+    def fibonacci(self, x1, x2, count, maxValue):
+
+        result = [x1,x2]
+        for i in range(count-2):
+            r = x1 + x2
+            result.append(r)
+            x1 = x2
+            x2 = r
+
+        if maxValue:
+            actualMax = max(map(abs, result))
+            if actualMax == 0.0:
+                return result
+            result = [x*maxValue/actualMax for x in result]
+
+        return result    
