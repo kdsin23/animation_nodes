d837e07ca7459142293acdf2bc18650dd75bd77c
harisreedhar <harisreedhar@users.noreply.github.com>
2020-04-26 11:02:38 +0530

    color mix 2 improvement

--- animation_nodes/nodes/bluefox_nodes/colormix2.py
+++ animation_nodes/nodes/bluefox_nodes/colormix2.py
@@ -1,9 +1,9 @@
 import bpy
 import numpy as np
 from bpy.props import *
-from ... events import executionCodeChanged, propertyChanged
 from ... base_types import AnimationNode, VectorizedSocket
-from ... data_structures import Color, ColorList, VirtualColorList, VirtualDoubleList
+from ... events import executionCodeChanged, propertyChanged
+from ... data_structures import Color, ColorList, VirtualColorList, VirtualDoubleList, DoubleList
 
 colormodeItems = [
     ("MIX", "Mix", "Mix", "", 0),
@@ -28,45 +28,31 @@ class Colormix2(bpy.types.Node, AnimationNode):
 
     clamp = BoolProperty(name = "Clamp", default = True, update = propertyChanged)    
 
-    usecolor1List: VectorizedSocket.newProperty()
-    usecolor2List: VectorizedSocket.newProperty()
+    usecolorAList: VectorizedSocket.newProperty()
+    usecolorBList: VectorizedSocket.newProperty()
     usefactorList: VectorizedSocket.newProperty()    
 
     def create(self):
-        self.newInput(VectorizedSocket("Color", "usecolor1List",
+        self.newInput(VectorizedSocket("Color", "usecolorAList",
             ("Color A", "colorA"), ("Colors A", "colorsA")))
-        self.newInput(VectorizedSocket("Color", "usecolor2List",
+        self.newInput(VectorizedSocket("Color", "usecolorBList",
             ("Color B", "colorB"), ("Colors B", "colorsB")))
         self.newInput(VectorizedSocket("Float", "usefactorList",
             ("Factor", "factor"), ("Factors", "factors")))        
         self.newInput("Float", "Alpha", "alpha", value = 1.0, hide = True)     
 
-        self.newOutput(VectorizedSocket("Color", ["usecolor1List", "usecolor2List", "usefactorList"],
+        self.newOutput(VectorizedSocket("Color", ["usecolorAList", "usecolorBList", "usefactorList"],
             ("Color", "color"), ("Colors", "colors")))   
 
     def draw(self, layout):
         layout.prop(self, "clamp")
         layout.prop(self, "mode")
+                   
+    def execute(self, colorsA, colorsB, factors, alpha):
+        if not self.usecolorAList: colorsA = ColorList.fromValues([colorsA])
+        if not self.usecolorBList: colorsB = ColorList.fromValues([colorsB])
+        if not self.usefactorList: factors = DoubleList.fromValue(factors)
 
-    def getExecutionFunctionName(self):
-        if self.usecolor1List and self.usecolor2List and self.usefactorList :
-            return "execute_allList"
-        elif self.usecolor1List and self.usecolor2List == 0 and self.usefactorList == 0 :
-            return "execute_onlyColAList"
-        elif self.usecolor1List == 0 and self.usecolor2List and self.usefactorList == 0 :
-            return "execute_onlyColBList"
-        elif self.usecolor1List == 0 and self.usecolor2List ==0 and self.usefactorList :
-            return "execute_onlyFactorList"
-        elif self.usecolor1List and self.usecolor2List ==0 and self.usefactorList :
-            return "execute_ColA_Factor_isList"
-        elif self.usecolor1List == 0 and self.usecolor2List and self.usefactorList :
-            return "execute_ColB_Factor_isList"
-        elif self.usecolor1List and self.usecolor2List and self.usefactorList == 0 :
-            return "execute_ColA_ColB_isList"
-        else:
-            return "execute_allSingle"                        
-
-    def execute_allList(self, colorsA, colorsB, factors, alpha):
         if len(colorsA) == 0 or len(colorsB) == 0 or len(factors) == 0:
             self.raiseErrorMessage("Reconnect Output Socket")
             return ColorList()
@@ -74,16 +60,14 @@ class Colormix2(bpy.types.Node, AnimationNode):
         if len(colorsA) == len(colorsB) == len(factors):
             colA = np.array(colorsA)
             colB = np.array(colorsB)
-            shaped_factor = np.repeat(factors, 4).reshape(-1, 4)
+            factor = np.array(factors)
         else:
             maxLength = max(max(len(colorsA), len(colorsB)), len(factors))
             colA = np.array(VirtualColorList.create(colorsA, colorsA[-1]).materialize(maxLength))
             colB = np.array(VirtualColorList.create(colorsB, colorsB[-1]).materialize(maxLength))
             factor = np.array(VirtualDoubleList.create(factors, factors[-1]).materialize(maxLength))
-            shaped_factor = np.repeat(factor, 4).reshape(-1, 4)   
+
         mode = self.mode
-        if self.clamp:
-            shaped_factor = np.clip(shaped_factor, 0.00, 1.00)
 
         if mode == "ADD":
             result = colA + colB
@@ -103,43 +87,23 @@ class Colormix2(bpy.types.Node, AnimationNode):
             else:
                 result = 1 - 2 * (1 - colA) * (1 - colB)
         elif mode == "MIX":
-            result = colB        
+            result = colB
 
-        out = self.color_mix(colA, result, shaped_factor)
+        out = self.color_mix(colA, result, factor)
         out[:,-1] = alpha
+
         if self.clamp:
-            out = np.clip(out, 0.00, 1.00)       
-        return out.tolist()           
+            out = np.clip(out, 0.00, 1.00)
+   
+        if self.usecolorAList == 0 and self.usecolorBList == 0 and self.usefactorList == 0:
+            return ColorList.fromValues(out)[0]
+        else:
+            return ColorList.fromValues(out)           
          
     def color_mix(self, colorsA, colorsB, factor):
-        return (1 - factor) * colorsA + factor * colorsB
-
-    def execute_onlyColAList(self, colorsA, colorB, factor, alpha):
-        colorsB = ColorList.fromValues([colorB])
-        return self.execute_allList(colorsA, colorsB, [factor], alpha)
-
-    def execute_onlyColBList(self, colorA, colorsB, factor, alpha):
-        colorsA = ColorList.fromValues([colorA])
-        return self.execute_allList(colorsA, colorsB, [factor], alpha)
-
-    def execute_onlyFactorList(self, colorA, colorB, factors, alpha):
-        colorsA = ColorList.fromValues([colorA])
-        colorsB = ColorList.fromValues([colorB])
-        return self.execute_allList(colorsA, colorsB, factors, alpha)
-
-    def execute_ColA_Factor_isList(self, colorsA, colorB, factors, alpha):
-        colorsB = ColorList.fromValues([colorB])
-        return self.execute_allList(colorsA, colorsB, factors, alpha) 
-
-    def execute_ColB_Factor_isList(self, colorA, colorsB, factors, alpha):
-        colorsA = ColorList.fromValues([colorA])
-        return self.execute_allList(colorsA, colorsB, factors, alpha)
-
-    def execute_ColA_ColB_isList(self, colorsA, colorsB, factor, alpha):
-        return self.execute_allList(colorsA, colorsB, [factor], alpha)
-
-    def execute_allSingle(self, colorA, colorB, factor, alpha):
-        colorsA = ColorList.fromValues([colorA])
-        colorsB = ColorList.fromValues([colorB])
-        result = self.execute_allList(colorsA, colorsB, [factor], alpha)
-        return result[0] 
+        shaped_factor = np.repeat(factor, 4).reshape(-1, 4)
+        if self.clamp:
+            shaped_factor = np.clip(shaped_factor, 0.00, 1.00) 
+        return (1 - shaped_factor) * colorsA + shaped_factor * colorsB
+    
+   
