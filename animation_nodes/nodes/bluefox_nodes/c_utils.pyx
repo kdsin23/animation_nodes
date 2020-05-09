45b9f7455ecfb3720d007fb487da3257207b5222
harisreedhar <harisreedhar@users.noreply.github.com>
2020-05-09 21:33:11 +0530

    Added Step effector

--- animation_nodes/nodes/bluefox_nodes/c_utils.pyx
+++ animation_nodes/nodes/bluefox_nodes/c_utils.pyx
@@ -5,7 +5,7 @@ from ... data_structures cimport (
     VirtualVector3DList, VirtualEulerList, VirtualFloatList, VirtualDoubleList,
     Action, ActionEvaluator, PathIndexActionChannel,
     BoundedAction, BoundedActionEvaluator,Color,
-    ColorList, PolygonIndicesList, IntegerList, PolySpline, BezierSpline
+    ColorList, PolygonIndicesList, IntegerList, PolySpline, BezierSpline, Interpolation
 )
 from ... math cimport (
     Vector3, Euler3, Matrix4, toMatrix4,toVector3,
@@ -20,6 +20,7 @@ from ... math cimport (
 from..matrix.c_utils import* 
 from mathutils import Matrix, Euler, Vector
 from math import *
+from ... nodes.number.c_utils import range_DoubleList_StartStop, mapRange_DoubleList_Interpolated 
 from ... algorithms.random import getRandom3DVector
 from ... algorithms.random import uniformRandomDoubleWithTwoSeeds
 from ... math cimport abs as absNumber
@@ -153,8 +154,28 @@ def inheritanceCurve(Vector3DList vA, Vector3DList vB, Vector3DList pathPoints, 
         newParameter = min(max(parameter, 0), 1)
         newParameter = newSpline.toUniformParameter(newParameter)
         newPoints[i] = newSpline.evaluatePoint(newParameter)
+    return newPoints 
 
-    return newPoints        
-
-
-
+def stepEffector(Matrix4x4List matrices, Vector3DList v, EulerList e, Vector3DList s, DoubleList influences, 
+                        Interpolation interpolation, double minValue, double maxValue):
+    cdef Vector3DList tA = extractMatrixTranslations(matrices)
+    cdef EulerList rA = extractMatrixRotations(matrices)
+    cdef Vector3DList sA = extractMatrixScales(matrices)
+    cdef int count = matrices.getLength()
+    cdef DoubleList strengths = range_DoubleList_StartStop(count, 0.00, 1.00)
+    cdef DoubleList interpolatedStrengths = mapRange_DoubleList_Interpolated(strengths, interpolation, 0, 1, minValue, maxValue)
+    cdef VirtualVector3DList translations_out = VirtualVector3DList.create(tA, (0, 0, 0))
+    cdef VirtualEulerList rotations_out = VirtualEulerList.create(rA, (0, 0, 0))
+    cdef VirtualVector3DList scales_out = VirtualVector3DList.create(sA, (1, 1, 1))
+    for i in range(count):
+        strength = interpolatedStrengths.data[i]
+        translations_out.get(i).x = tA.data[i].x + influences.data[i] * v.data[0].x * strength
+        translations_out.get(i).y = tA.data[i].y + influences.data[i] * v.data[0].y * strength
+        translations_out.get(i).z = tA.data[i].z + influences.data[i] * v.data[0].z * strength
+        rotations_out.get(i).x = rA.data[i].x + influences.data[i] * e.data[0].x * strength
+        rotations_out.get(i).y = rA.data[i].y + influences.data[i] * e.data[0].y * strength
+        rotations_out.get(i).z = rA.data[i].z + influences.data[i] * e.data[0].z * strength
+        scales_out.get(i).x = sA.data[i].x + influences.data[i] * s.data[0].x * strength
+        scales_out.get(i).y = sA.data[i].y + influences.data[i] * s.data[0].y * strength 
+        scales_out.get(i).z = sA.data[i].z + influences.data[i] * s.data[0].z * strength      
+    return composeMatrices(count, translations_out, rotations_out, scales_out), interpolatedStrengths           
