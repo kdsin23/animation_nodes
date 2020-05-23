07807a7228210de815a67108bf44749c9d0cfd50
harisreedhar <harisreedhar@users.noreply.github.com>
2020-05-23 13:06:22 +0530

    Removed None mode
    None mode automatically calls when child is removed

--- animation_nodes/nodes/bluefox_nodes/dupliInstancer.py
+++ animation_nodes/nodes/bluefox_nodes/dupliInstancer.py
@@ -1,14 +1,15 @@
 import bpy
 from bpy.props import *
 from ... events import propertyChanged
-from ... base_types import AnimationNode, VectorizedSocket
+from ... base_types import AnimationNode
 
 dupliModeItems = [
     ("VERTS", "Vertices", "Instance on vertices", "", 0),
     ("FACES", "Faces", "Instance on Faces", "", 1),
-    ("COLLECTION", "Collection", "Instance a collection", "", 2),
-    ("NONE", "None", "Clear Parent", "", 3)
+    ("COLLECTION", "Collection", "Instance a collection", "", 2)
 ]
+
+lastChild = {}
 
 class DupliInstancerNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_DupliInstancer"
@@ -16,12 +17,10 @@ class DupliInstancerNode(bpy.types.Node, AnimationNode):
     errorHandlingType = "EXCEPTION"
     bl_width_default = 160
 
-    __annotations__ = {}
+    useDisplay : BoolProperty(name = "Display Instancer", default = True, update = propertyChanged)
+    useRender : BoolProperty(name = "Render Instancer", default = True, update = propertyChanged)
 
-    __annotations__["useDisplay"] = BoolProperty(name = "Display Instancer", default = True, update = propertyChanged)
-    __annotations__["useRender"] = BoolProperty(name = "Render Instancer", default = True, update = propertyChanged)
-
-    __annotations__["mode"] = EnumProperty(name = "Mode", default = "VERTS",
+    mode : EnumProperty(name = "Mode", default = "VERTS",
         items = dupliModeItems, update = AnimationNode.refresh)
 
     def create(self):
@@ -35,15 +34,15 @@ class DupliInstancerNode(bpy.types.Node, AnimationNode):
             self.newInput("Object", "Child", "child")
             self.newInput("Boolean", "Scale by Face", "scaleByFace")
             self.newInput("Float", "Factor", "factor", value = 1.0)
-        elif self.mode == "NONE":
-            self.newInput("Object", "Child", "child")
 
         self.newOutput("Object", "Parent", "object")
 
     def draw(self, layout):
-        layout.prop(self, "mode")
-        layout.prop(self, "useDisplay")
-        layout.prop(self, "useRender")
+        row = layout.row(align = True)
+        subrow = row.row(align = True)
+        subrow.prop(self, "mode", text = "")
+        subrow.prop(self, "useDisplay", index = 1, text = "", icon = "RESTRICT_VIEW_OFF")
+        subrow.prop(self, "useRender", index = 2, text = "", icon = "RESTRICT_RENDER_OFF")
 
     def getExecutionFunctionName(self):
         if self.mode == "VERTS":
@@ -51,47 +50,46 @@ class DupliInstancerNode(bpy.types.Node, AnimationNode):
         elif self.mode == "FACES":
             return "execute_Faces"
         elif self.mode == "COLLECTION":
-            return "execute_Collection" 
-        elif self.mode == "NONE":
-            return "execute_None"                 
+            return "execute_Collection"                 
 
     def execute_Verts(self, parent, child, align):
+        parentOut = parent
         if parent == None or child == None or parent == child:
-            return parent
+            if parent != None and parent.instance_type:
+                parent.instance_type = "NONE"   
         else:
             child.parent = parent
             parent.instance_type = "VERTS"
             parent.show_instancer_for_viewport = self.useDisplay
             parent.show_instancer_for_render = self.useRender
             parent.use_instance_vertices_rotation = align
+        return parentOut
 
     def execute_Faces(self, parent, child, scaleByFace, factor):
+        parentOut = parent
         if parent == None or child == None or parent == child:
-            return parent
+            if parent != None and parent.instance_type:
+                parent.instance_type = "NONE"
         else:
             child.parent = parent
             parent.instance_type = "FACES"
             parent.show_instancer_for_viewport = self.useDisplay
             parent.show_instancer_for_render = self.useRender
             parent.use_instance_faces_scale = scaleByFace
-            parent.instance_faces_scale = factor          
+            parent.instance_faces_scale = factor
+        return parentOut
 
     def execute_Collection(self, parent, collection):
+        parentOut = parent
         if parent == None or collection == None:
-            return parent
+            if parent != None and parent.instance_type:
+                parent.instance_type = "NONE"
         else:
             try:
                 parent.instance_type = "COLLECTION"
                 parent.show_instancer_for_viewport = self.useDisplay
                 parent.show_instancer_for_render = self.useRender
-                parent.instance_collection = collection
-                return parent    
+                parent.instance_collection = collection   
             except TypeError:
                 self.raiseErrorMessage("Parent should be an Empty")
-                return parent  
-
-    def execute_None(self, parent, child):
-        if parent == None or child == None or parent == child:
-            return parent
-        else:
-            child.parent = None                              
+        return parentOut      
