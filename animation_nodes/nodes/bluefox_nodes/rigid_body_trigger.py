00a0c5e7ecc96f89b7ae099b1aecbd11739643b4
Harisreedhar <harisreedhar@users.noreply.github.com>
2020-06-30 21:41:09 +0530

    Update rigid_body_trigger.py

--- animation_nodes/nodes/bluefox_nodes/rigid_body_trigger.py
+++ animation_nodes/nodes/bluefox_nodes/rigid_body_trigger.py
@@ -1,29 +1,63 @@
 import bpy
 from bpy.props import *
-from ... data_structures import Vector3DList
 from ... utils.depsgraph import getEvaluatedID
 from ... base_types import AnimationNode, VectorizedSocket
+from ... data_structures import Vector3DList, BooleanList, DoubleList
+
+collisionShapeItems = [
+    ("BOX", "Box", "", "NONE", 0),
+    ("SPHERE", "Sphere", "", "NONE", 1),
+    ("CAPSULE", "Capsule", "", "NONE", 2),
+    ("CYLINDER", "Cylinder", "", "NONE", 3),
+    ("CONE", "Cone", "", "NONE", 4),
+    ("CONVEX_HULL", "Convex Hull", "", "NONE", 5),
+    ("MESH", "Mesh", "", "NONE", 6)    
+]
 
 class RigidBodyTrigger(bpy.types.Node, AnimationNode):
     bl_idname = "an_RigidBodyTrigger"
     bl_label = "Rigid Body Trigger"
     errorHandlingType = "EXCEPTION"
 
+    collisionShape: EnumProperty(name = "Collision Shape", default = "CONVEX_HULL",
+        items = collisionShapeItems, update = AnimationNode.refresh)
+
     useObjectList: VectorizedSocket.newProperty()
+    useDynamicsList: VectorizedSocket.newProperty()
+    useMassList: VectorizedSocket.newProperty()
+    useFrictionList: VectorizedSocket.newProperty()
+    useBouncinessList: VectorizedSocket.newProperty()
+
     linkObject: BoolProperty(name = "Link Object", description = "link objects to rigid body collection", default = True, update = AnimationNode.refresh)
-    enableLink: BoolProperty(name = "Enable Link", description = "Disabling this option will stop linking on every execution", default = True, update = AnimationNode.refresh)
+    enableLink: BoolProperty(name = "Is Used", default = True, update = AnimationNode.refresh)
+    enableShape: BoolProperty(name = "Is Used", default = True, update = AnimationNode.refresh)
+    useMoreSettings: BoolProperty(name = "More Settings", default = False, update = AnimationNode.refresh)
 
     def create(self):
         self.newInput("Scene", "Scene", "scene", hide = True)
         self.newInput(VectorizedSocket("Object", "useObjectList",
             ("Object", "object", dict(defaultDrawType = "PROPERTY_ONLY")),
-            ("Objects", "objects")))
+            ("Objects", "objects")))   
         self.newInput("Falloff", "Falloff", "falloff")
         self.newInput("Float", "Threshold", "threshold", value = 0.5)
+        if self.useMoreSettings:    
+            self.newInput(VectorizedSocket("Boolean", "useDynamicsList",
+                ("Dynamic", "dynamic"),("Dynamics", "dynamics")))
+            self.newInput(VectorizedSocket("Float", "useMassList",
+                ("Mass", "mass"),("Masses", "masses")))
+            self.newInput(VectorizedSocket("Float", "useFrictionList",
+                ("Friction", "friction"),("Frictions", "frictions")))
+            self.newInput(VectorizedSocket("Float", "useBouncinessList",
+                ("Bounciness", "bounciness"),("Bouncinesses", "bouncinesses")))           
 
         self.newOutput(VectorizedSocket("Object", "useObjectList",
             ("Object", "objectOut", dict(defaultDrawType = "PROPERTY_ONLY")),
             ("Objects", "objectsOut")))
+
+        if self.useMoreSettings:
+            for i in range(4,8):    
+                self.inputs[i].useIsUsedProperty = True
+                self.inputs[i].isUsed = False
 
     def edit(self):
         inputSocket = self.inputs[0]
@@ -34,16 +68,35 @@ class RigidBodyTrigger(bpy.types.Node, AnimationNode):
             inputSocket.hide = True
 
     def draw(self, layout):
-        row = layout.row(align = True)
-        row.prop(self, "linkObject", text = "Link Objects", toggle = True)
-        row2 = row.row(align = True)
+        row1 = layout.row(align = True)
+        row1.prop(self, "linkObject", text = "Link Objects", toggle = True)
+        row2 = row1.row(align = True)
         testIcon = "LAYER_USED"
         if self.enableLink:
             testIcon = "LAYER_ACTIVE"
         row2.prop(self, "enableLink", text = "", icon = testIcon)
-        row.active = self.enableLink
+        row1.active = self.enableLink
+        
+        if self.useMoreSettings:
+            row3 = layout.row(align = True)
+            row3.prop(self, "collisionShape", text = "")
+            row4 = row3.row(align = True)
+            testIcon = "LAYER_USED"
+            if self.enableShape:
+                testIcon = "LAYER_ACTIVE"
+            row4.prop(self, "enableShape", text = "", icon = testIcon)
+            row3.active = self.enableShape
 
-    def execute(self, scene, objects, falloff, threshold):
+    def drawAdvanced(self, layout):
+        layout.prop(self, "useMoreSettings")     
+
+    def getExecutionFunctionName(self):
+        if not self.useMoreSettings:
+            return "execute_Simple"
+        else:
+            return "execute_MoreSettings"
+
+    def execute_Simple(self, scene, objects, falloff, threshold):
         if objects is None:
             return
         if not self.useObjectList:
@@ -54,32 +107,74 @@ class RigidBodyTrigger(bpy.types.Node, AnimationNode):
                 self.raiseErrorMessage("Rigid Body World not found")
             else:
                 locations = Vector3DList.fromValues([getEvaluatedID(object).location for object in objects])
-                self.rigidbody_falloff_animate(objects, rigidbody_world, locations, falloff, threshold)
-
-        return objects
-
-    def rigidbody_falloff_animate(self, objects, rigidbody_world, locations, falloff, threshold):
-        falloffEvaluator = self.getFalloffEvaluator(falloff)
-        influences = falloffEvaluator.evaluateList(locations)
-        
-        rb_collection = rigidbody_world.collection
-        if rb_collection is None:
-            self.setErrorMessage("Rigid Body Collection not found")
-        else:
-            rb_objects = rb_collection.objects
-            if self.enableLink:
-                self.link_Objects(objects, rb_objects)
-
-            for obj, influence in zip(objects, influences):
-                if obj.type != "MESH":
-                    self.setErrorMessage("Object is not a mesh object")
+                falloffEvaluator = self.getFalloffEvaluator(falloff)
+                influences = falloffEvaluator.evaluateList(locations)
+                
+                rb_collection = rigidbody_world.collection
+                if rb_collection is None:
+                    self.setErrorMessage("Rigid Body Collection not found")
                 else:
-                    try:
-                        obj.rigid_body.kinematic = True
-                        if threshold <= influence:
-                            obj.rigid_body.kinematic = False
-                    except:
-                        self.setErrorMessage("Link objects to rigid body collection")           
+                    if self.enableLink:
+                        self.link_Objects(objects, rb_collection.objects)
+
+                    for obj, influence in zip(objects, influences):
+                        if obj.type != "MESH":
+                            self.setErrorMessage("Object is not a mesh object")
+                        else:
+                            try:
+                                obj.rigid_body.kinematic = True
+                                if threshold <= influence:
+                                    obj.rigid_body.kinematic = False
+                            except:
+                                self.setErrorMessage("Link objects to rigid body collection")
+        return objects      
+
+    def execute_MoreSettings(self, scene, objects, falloff, threshold, dynamics, masses, frictions, bouncinesses):
+        if objects is None:
+            return
+        count = len(objects)
+        if not self.useObjectList: objects = [objects]
+        if not self.useDynamicsList: dynamics = self.fillList(dynamics, count, "boolean")
+        if not self.useMassList: masses = self.fillList(masses, count, "float")
+        if not self.useFrictionList: frictions = self.fillList(frictions, count, "float") 
+        if not self.useBouncinessList: bouncinesses = self.fillList(bouncinesses, count, "float")        
+    
+        if not scene is None:
+            rigidbody_world = scene.rigidbody_world
+            if rigidbody_world is None:
+                self.raiseErrorMessage("Rigid Body World not found")
+            else:
+                locations = Vector3DList.fromValues([getEvaluatedID(object).location for object in objects])
+                falloffEvaluator = self.getFalloffEvaluator(falloff)
+                influences = falloffEvaluator.evaluateList(locations)
+                rb_collection = rigidbody_world.collection
+
+                if rb_collection is None:
+                    self.setErrorMessage("Rigid Body Collection not found")
+                else:
+                    if self.enableLink:
+                        self.link_Objects(objects, rb_collection.objects)
+
+                    totalCount = len(dynamics) + len(masses) + len(frictions) + len(bouncinesses)
+                    if totalCount / 4 != count:
+                        self.setErrorMessage("Boolean list length mismatch")
+                    else:
+                        for i in range(count):
+                            if objects[i].type != "MESH":
+                                self.setErrorMessage("Object is not a mesh object")
+                            else:
+                                try:
+                                    if self.inputs[5].isUsed: objects[i].rigid_body.mass = masses[i]
+                                    if self.inputs[4].isUsed: objects[i].rigid_body.enabled = dynamics[i]
+                                    if self.inputs[6].isUsed: objects[i].rigid_body.friction = frictions[i]
+                                    if self.inputs[7].isUsed: objects[i].rigid_body.restitution = bouncinesses[i]
+                                    objects[i].rigid_body.kinematic = True
+                                    if threshold <= influences[i]:
+                                        objects[i].rigid_body.kinematic = False
+                                    if self.enableShape: objects[i].rigid_body.collision_shape = self.collisionShape    
+                                except:
+                                    self.setErrorMessage("Link objects to rigid body collection")
+        return objects
 
     def link_Objects(self, objects, rb_objects):
         for obj in objects:
@@ -88,6 +183,14 @@ class RigidBodyTrigger(bpy.types.Node, AnimationNode):
             else:
                 if obj.name in rb_objects: rb_objects.unlink(obj)
 
+    def fillList(self, value, count, datatype):
+        if datatype == "boolean":
+            b = BooleanList(length = count)
+        elif datatype == "float":
+            b = DoubleList(length = count)
+        b.fill(value)
+        return b
+       
     def getFalloffEvaluator(self, falloff):
         try: return falloff.getEvaluator("LOCATION")
         except: self.raiseErrorMessage("This falloff cannot be evaluated for vectors")
